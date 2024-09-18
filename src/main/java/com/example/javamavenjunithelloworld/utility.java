93b47b997255ea765ecbec80ad759c12c4bd802f
Nithin2234 <nithingarugula@gmail.com>
2024-09-18 11:32:58 +0530

    NITHINN0001-Added utility functions

---
+++ src/main/java/com/example/javamavenjunithelloworld/utility.java
@@ -0,0 +1,14 @@
+package com.example.javamavenjunithelloworld;
+
+public class utility {
+
+    // Function to check if a number is odd or even
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+
+     public static void nithin() {
+        System.out.println("I am Nithin");
+    }
+
+}
